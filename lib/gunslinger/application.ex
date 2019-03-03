16e3821316c7a944b21d7d997b915793b597e643
Gavin McDonald <gavinmcdoh@gmail.com>
2019-03-03 14:02:55 -0500

    instantiate Cowboy and Plug and set up a simple router

--- lib/gunslinger/application.ex
+++ lib/gunslinger/application.ex
@@ -1,19 +1,11 @@
 defmodule Gunslinger.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
     children = [
-      # Starts a worker by calling: Gunslinger.Worker.start_link(arg)
-      # {Gunslinger.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: Gunslinger.Router, options: [port: 4001]}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Gunslinger.Supervisor]
     Supervisor.start_link(children, opts)
   end
---
+++ lib/gunslinger/router.ex
@@ -0,0 +1,10 @@
+defmodule Gunslinger.Router do
+  use Plug.Router
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Routing!")
+  end
+end
