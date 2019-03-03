8a31e3975f91c5a413c428051671732ec7d27792
Gavin McDonald <gavinmcdoh@gmail.com>
2019-03-03 14:38:39 -0500

    added templates

--- lib/gunslinger/actions/posts/index.ex
+++ lib/gunslinger/actions/posts/index.ex
@@ -1,9 +1,12 @@
 defmodule Gunslinger.Actions.Posts.Index do
   import Plug.Conn
+  require EEx
+
+  EEx.function_from_file :def, :index, "lib/gunslinger/templates/posts/index.html.eex", [:adjective]
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    send_resp(conn, 200, "Keep plugging away!")
+    send_resp(conn, 200, index("coolest"))
   end
 end
