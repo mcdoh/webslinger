0db9db9865175e6bda97eba89b6c7fd5c32af233
Gavin McDonald <gavinmcdoh@gmail.com>
2019-03-03 14:46:12 -0500

    leverage EEx's 'assigns' so that we can just pass in a keyword list

--- lib/gunslinger/actions/posts/index.ex
+++ lib/gunslinger/actions/posts/index.ex
@@ -2,11 +2,11 @@ defmodule Gunslinger.Actions.Posts.Index do
   import Plug.Conn
   require EEx
 
-  EEx.function_from_file :def, :index, "lib/gunslinger/templates/posts/index.html.eex", [:adjective]
+  EEx.function_from_file :def, :index, "lib/gunslinger/templates/posts/index.html.eex", [:assigns]
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    send_resp(conn, 200, index("coolest"))
+    send_resp(conn, 200, index(adjective1: "coolest", adjective2: "fastest"))
   end
 end
